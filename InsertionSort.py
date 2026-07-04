4594c45e1e13f1fae049456f7257a2be3d52ca21
arun12376 <YOUR_ARUN12376_akumar48204@ucumberlands.edu>
2026-07-04 11:19:20 -0400

    Third commit with comment

--- InsertionSort.py
+++ InsertionSort.py
@@ -7,7 +7,7 @@ def insertion_sort_monotonically_decreasing(A):
     for j in range(1, n):
         key = A[j]
         i = j - 1
-
+    #  Only looking for elements LESS than the key to slide them right
         while i >= 0 and A[i] < key:
             A[i + 1] = A[i]
             i = i - 1
@@ -18,8 +18,7 @@ def insertion_sort_monotonically_decreasing(A):
 
 
 if __name__ == "__main__":
-    # arr = [31, 23, 17, 14, 9, 5, 2]
     arr = [31, 23, 17, 14, 9, 5, 2]
     print("Original given array (monotonically decreasing):", arr)
     sorted_arr = insertion_sort_monotonically_decreasing(arr)
-    print("Sorted array via insertion sort :", sorted_arr)
+    print("Monotonically Decresaing Sorted array via insertion sort  :", sorted_arr)
